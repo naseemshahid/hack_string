312e686fe9bad523f6232996ba2bc5e125b0fde7
naseemshahid <naseemshahid69@gmail.com>
2023-12-19 21:08:35 +0500

    question no 1

--- string_utlis.h
+++ string_utlis.h
@@ -7,3 +7,36 @@ void replaceChar(char *s, char oldChar, char newChar);
 char *replaceCharCopy(char *s, char oldChar, char newChar,char *ns);
 void removeChar(char *s, char c);
 char * removeCharCopy( char *s, char c);
+
+
+//question no 1
+
+void replaceChar(char *s, char oldChar, char newChar){
+
+char *testch=s;
+
+while(*s != '\0'){
+
+if(*s== oldChar){
+
+*s = newChar;
+
+}
+
+s++;
+
+}
+
+printf("\nAfter modification the result is : ");
+
+while ( testch != (s+1) ){
+    
+    printf("%c",*testch);
+    testch++;
+
+}
+
+
+}
+
+// question 2
