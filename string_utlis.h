d9b1fd6d3735511b4b8157a14f29bcb1d63d5d2f
naseemshahid <naseemshahid69@gmail.com>
2023-12-19 20:51:16 +0500

    including header file and prototypes

---
+++ string_utlis.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string_utlis.h>
+
+
+void replaceChar(char *s, char oldChar, char newChar);
+char *replaceCharCopy(char *s, char oldChar, char newChar,char *ns);
+void removeChar(char *s, char c);
+char * removeCharCopy( char *s, char c);
